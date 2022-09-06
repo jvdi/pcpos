0102579a9f4e5d88cab329fd8173ff3c2fff0706
Mohammad Javidi <m.javidii@yahoo.com>
2022-09-06 16:50:45 +0430

    Change - TrayIcon Icon

--- gui_for_tray_icon.py
+++ gui_for_tray_icon.py
@@ -5,7 +5,7 @@ from PIL import Image
 # TrayIcon for show app is run
 class TrayIcon():
     def __init__(self):
-        self.image = Image.open('assets/tray.ico')
+        self.image = Image.open('assets/pos.ico')
         self.icon = pystray.Icon('PCPosAPI', self.image, 'PCPosAPI is running', None)
 
     def run_detached(self):
