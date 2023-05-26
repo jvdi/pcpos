391be3716bc16b3beeac2a92158769b1a7355c5b
Mohammad Javidi <m.javidii@yahoo.com>
2023-05-26 12:21:37 +0330

    Add - sub-menu for trayicon

--- gui_for_tray_icon.py
+++ gui_for_tray_icon.py
@@ -1,4 +1,4 @@
-import pystray
+import pystray, webbrowser
 from PIL import Image
 
 
@@ -6,11 +6,14 @@ from PIL import Image
 class TrayIcon():
     def __init__(self):
         self.image = Image.open('assets/pos.ico')
-        self.icon = pystray.Icon('PCPosAPI', self.image, 'PCPosAPI is running', None)
+        self.menu = (
+            pystray.MenuItem('درباره نرم افزار', lambda: webbrowser.open('https://github.com/jvdi/rahkar-pcpos')),
+            pystray.MenuItem('بستن سرویس', lambda: [self.stop()])
+            )
+        self.icon = pystray.Icon('PCPosAPI', self.image, 'PCPosAPI is running', self.menu)
 
     def run_detached(self):
         self.icon.run_detached()
 
     def stop(self):
         self.icon.stop()
-        exit()
